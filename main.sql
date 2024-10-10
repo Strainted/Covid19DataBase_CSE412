ea9567884910224eb2a044dc45bcad21dd232eda
Luke <lengelb03@gmail.com>
2024-10-09 17:37:06 -0700

    added all tables and revised keys and foriegn keys

--- main.sql
+++ main.sql
@@ -1,23 +1,58 @@
-CREATE TABLE Covid19 {
-    total_id SERIAL PRIMARY KEY,
+CREATE TABLE Covid19 (
+    covid_id SERIAL PRIMARY KEY,
     total_cases INT,
     total_deaths INT,
     total_recoveries INT,
     total_active_cases INT,
     total_new_cases INT
-};
+);
 
-CREATE TABLE Account {
+
+CREATE TABLE Country (
+    country_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    num_cases INT,
+    num_deaths INT,
+    num_recoveries INT,
+    num_active_cases INT,
+    num_new_cases INT,
+    covid_id INT,
+    FOREIGN KEY (covid_id) REFERENCES Covid19(covid_id)
+);
+
+
+CREATE TABLE Account (
     username VARCHAR(20) PRIMARY KEY,
-    graph_id 
     password VARCHAR(20) NOT NULL,
-    favorite_visualization 
-}
+    language VARCHAR(20)
+);
 
-CREATE TABLE Graph {
+
+CREATE TABLE Graph (
     graph_id SERIAL PRIMARY KEY,
-    FOREIGN KEY (username) REFERENCES Account(username)
+    username VARCHAR(20), 
     style INT,
     min_time TIMESTAMP NOT NULL,
-    max_time TIMESTAMP NOT NULL
-};
+    max_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (username) REFERENCES Account(username) ON DELETE CASCADE
+);
+
+
+CREATE TABLE FavoriteVisualizations (
+    username VARCHAR(20), 
+    graph_id INT,       
+    PRIMARY KEY (username, graph_id),
+    FOREIGN KEY (username) REFERENCES Account(username) ON DELETE CASCADE,
+    FOREIGN KEY (graph_id) REFERENCES Graph(graph_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Case (
+    case_id SERIAL PRIMARY KEY,
+    location VARCHAR(50),
+    time TIMESTAMP,
+    fatal BOOLEAN,
+    country_id INT, 
+    FOREIGN KEY (country_id) REFERENCES Country(country_id)
+);
+
