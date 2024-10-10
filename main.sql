c1e6c119a2ec4d2f05a89bf87ddcf2ee1371ef4b
Luke <lengelb03@gmail.com>
2024-10-09 17:18:27 -0700

    added Covid19, Account, and Graph Tables

--- main.sql
+++ main.sql
@@ -1,4 +1,23 @@
 CREATE TABLE Covid19 {
-    totalCases INT,
-    totalDeaths INT 
+    total_id SERIAL PRIMARY KEY,
+    total_cases INT,
+    total_deaths INT,
+    total_recoveries INT,
+    total_active_cases INT,
+    total_new_cases INT
+};
+
+CREATE TABLE Account {
+    username VARCHAR(20) PRIMARY KEY,
+    graph_id 
+    password VARCHAR(20) NOT NULL,
+    favorite_visualization 
 }
+
+CREATE TABLE Graph {
+    graph_id SERIAL PRIMARY KEY,
+    FOREIGN KEY (username) REFERENCES Account(username)
+    style INT,
+    min_time TIMESTAMP NOT NULL,
+    max_time TIMESTAMP NOT NULL
+};
